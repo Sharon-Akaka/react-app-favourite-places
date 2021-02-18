9b709a0c28472ba1b404441b41fbd3680c0c1cef
Sharon Akaka <sharon.akaka@gmail.com>
2021-02-18 14:56:22 +0000

    Resolved target error in a tag

--- src/components/Places.tsx
+++ src/components/Places.tsx
@@ -15,7 +15,7 @@ function Places(props: BodyProps) {
         <div className='containerHolder'>
             <div className='container'>
                 <img src={props.img} alt={props.alt} />
-                <h2>{props.cityName}, {props.countryName}(<a href={props.mapLink} target="_blank">map link</a>)</h2>
+                <h2>{props.cityName}, {props.countryName}(<a href={props.mapLink} rel="noreferrer" target="_blank">map link</a>)</h2>
                 <h3>{props.title}</h3>
                 <p>{props.paragraph}</p>
             </div>
